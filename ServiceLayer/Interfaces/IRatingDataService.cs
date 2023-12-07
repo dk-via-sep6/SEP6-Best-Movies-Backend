d1cfc800a49a2aac30765cbdf896c4e196fd9a31
Vlad Lazar <lazar.vlad151@gmail.com>
2023-12-07 14:33:00 +0100

    added endpoint for fetching the user ratings with the movie id

--- ServiceLayer/Interfaces/IRatingDataService.cs
+++ ServiceLayer/Interfaces/IRatingDataService.cs
@@ -11,5 +11,6 @@ namespace ServiceLayer.Interfaces
         Task<IEnumerable<RatingDomain>> GetRatingsByUserIdAsync(string userId);
         Task UpdateRatingAsync(RatingDomain rating);
         Task DeleteRatingAsync(int id);
+        Task<RatingDomain> GetRatingByUserAndMovieAsync(string userId, int movieId);
     }
 }
